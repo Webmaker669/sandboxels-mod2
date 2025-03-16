3c24192aba763211d4be54b3c3a0f2ab389a0254
Webmaker669 <ethynh2010@gmail.com>
2025-03-15 21:09:49 -0400

    Create Mod.js

---
+++ Mod.js
@@ -0,0 +1,16 @@
+elements.fireball = {
+  color: "#ff4500",
+  behavior: function(pixel, x, y) {
+    // Fireball moves right
+    pixel.move(1, 0);
+    // If the fireball hits water, turn into steam
+    if (pixel.isTouching("water")) {
+      pixel.type = "steam";
+    }
+    // If the fireball touches anything solid, stop moving
+    if (pixel.isTouching("stone")) {
+      pixel.move(0, 0);
+    }
+  },
+  temperature: 500, // Fireball is hot!
+};
